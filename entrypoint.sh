75f246997b3f20d69fdb6f664e4ce123a44f1dd3
Loup <90267658+Loup-Garou911XD@users.noreply.github.com>
2025-03-21 03:43:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -R /home/me/* /home/container
+mv /home/me/* /home/container
 cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
