b94841eefb79beacb1db8d8cd79169248cc00fc2
Loup <90267658+Loup-Garou911XD@users.noreply.github.com>
2025-03-20 22:29:10 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
-
+#!/bin/bash
+cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
 echo okayyyyyyyyy
-ls -al /home/container
-/home/container/ballisticakit_server
+ls -al
+./ballisticakit_server
