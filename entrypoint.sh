adee64e2d9afeef62409507b8eccfb14d64c5b29
Loup-Garou911XD <Loup-Garou911XD@users.noreply.github.com>
2025-03-21 03:04:29 +0530

    asff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /home/me
+cp /home/me /home/container -R
+cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
 echo okayyyyyyyyy
 pwd
 ls -al
-ls -al /home/me
 ./ballisticakit_server
