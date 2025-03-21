fb47c58202cf526b2d19e30df57b0881c450e741
Mikahael <54455412+Mikahael@users.noreply.github.com>
2025-03-21 12:24:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
 echo hewllow_worldd
-curl -O https://raw.githubusercontent.com/Mikahael/ballistica/refs/heads/main/src/assets/server_package/config.toml
 pwd
 ls -al
 ./ballisticakit_server
